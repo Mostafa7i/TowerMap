b6113b3e602ced54be01b3883718f4b1bf3d7750
Mostafa Mahmoud <mostasafi7@gmail.com>
2026-02-01 23:11:18 +0200

    fix tailwind line 74

--- src/app/page.js
+++ src/app/page.js
@@ -49,10 +49,7 @@ export default function Home() {
                 <div className="absolute inset-0 bg-linear-to-r from-fuchsia-600 to-violet-600 opacity-0 group-hover:opacity-100 transition-opacity"></div>
               </button>
 
-              <button
-                className="px-10 py-5 bg-white/10 backdrop-blur-md border-2 border-white/20 text-white rounded-2xl font-bold text-lg transition-all duration-300
-               hover:bg-white/20 hover:border-white/40 hover:scale-105"
-              >
+              <button className="px-10 py-5 bg-white/10 backdrop-blur-md border-2 border-white/20 text-white rounded-2xl font-bold text-lg transition-all duration-300 hover:bg-white/20 hover:border-white/40 hover:scale-105">
                 عرض توضيحي
               </button>
             </div>
