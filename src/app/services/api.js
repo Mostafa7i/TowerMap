9de16a56b76a1283c7386c9c8f47821a061ce7dc
Mostafa Mahmoud <mostasafi7@gmail.com>
2026-02-25 04:04:42 +0200

    update api page

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -5,7 +5,7 @@ import { NotifiyErorr } from "../components/Notify";
 const API = axios.create({
     // baseURL : "http://localhost:5000/api",
     baseURL : process.env.NODE_ENV === "production" 
-    ? "https://your-backend.vercel.app/api" 
+    ? "https://tower-map-back-end.vercel.app/api" 
     : "http://localhost:5000/api",
     headers :{
         'Content-Type' : 'application/json'
